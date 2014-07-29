e39738fd53becbf0ecc947b5206059c85ed4ae0d
CHoef <choef@wellesley.edu>
2014-07-29 17:14:03 -0400

    Level 1 to 2
    Pieces can now be dragged from Level 1 to Level 2. Much work needs to
    be done and the coding is kind of unpredictable in the sense that I
    don't know why it is working.

--- CHoef_MoClo/Parts.js
+++ CHoef_MoClo/Parts.js
@@ -6,9 +6,9 @@ $(function(){
    	 
 	//Various classes of draggable objects sorted into the different tabs. 
 	//These pieces will only stick to things that they're meant to due to revert
-	var greens = $(".greens").draggable({zIndex: 100, revert: "invalid"});
-	var toppings = $(".toppings").draggable({zIndex: 100, revert: "invalid"});
-	var dressing = $(".dressing").draggable({zIndex: 100, revert: "invalid"});
+	var greens = $(".greens").draggable({zIndex: 2, revert: "invalid"});
+	var toppings = $(".toppings").draggable({zIndex: 2, revert: "invalid"});
+	var dressing = $(".dressing").draggable({zIndex: 2, revert: "invalid"});
 	
 	
 	//The drop class contains draggable items. It snaps them into place, then prevents them from moving again.
@@ -23,9 +23,12 @@ $(function(){
                                            .css({position:'relative'});
         	//$("#drop").append($("#greens"));
         	//$(ui.draggable).draggable("disabled", "true");
-        	$(this).css("z-index", "0");
+        	//$(this).css("z-index", "0");
         }
 		});
+		
+		
+	
 		
 	//This prevents draggables from being dropped in the wrong place
 	$("#greenDrop").droppable("option","accept",".greens");
@@ -33,43 +36,62 @@ $(function(){
 	$("#dressingDrop").droppable("option","accept",".dressing");
 	
 	
+	
 	$("#comboButton").click( function(){
+		
 		//this needs work to place it in the right spot
 		var greenImg = $("#greenDrop").find("img");
 		var topImg = $("#toppingsDrop").find("img");
 		var dressImg = $("#dressingDrop").find("img");
-		console.log(greenImg.get(1));
-		console.log(topImg.get(0));
-		console.log(dressImg.length);
-		//$("#greenDrop img").clone().appendTo("#combos").css({position:'relative', left:'0px', top:'0px'});
-		//$("#toppingsDrop img").clone().appendTo("#combos");
-		//$("#dressingDrop img").clone().appendTo("#combos");					
-		//$("#greenDrop img").clone().appendTo("body");
-		//var i = greenImg.length;
-		//var j = topImg.length;
-		//var k = dressImg.length;
+		
 		var x = 0;
 		for(var i = greenImg.length; i>0; i--){
 			for(var j = topImg.length; j>0; j--){
 				for(var k = dressImg.length; k>0; k--){
-			$("#combos").append("<div class = 'lvl1Perm' id='perm'"+x+"></div>");
-			console.log(i);
-			console.log(j);
-			console.log(k);
-			$("#greenDrop img:nth-child("+i+")").clone().appendTo("#combos");
-			$("#toppingsDrop img:nth-child("+j+")").clone().appendTo("#combos");
-			$("#dressingDrop img:nth-child("+k+")").clone().appendTo("#combos");
+			$("#combos").append("<div class ='lvl1Perm' id='perm"+x+"'></div>");
+			$(".lvl1Perm").draggable({
+       			containment: 'document',
+       			helper: 'clone',
+       			zIndex:10000,
+       			//appendTo: "#lvl2Drop"
+   			});
+			var gi = $("#greenDrop img:nth-child("+i+")").clone().appendTo("#perm"+x);
+			var ti = $("#toppingsDrop img:nth-child("+j+")").clone().appendTo("#perm"+x);
+			var di = $("#dressingDrop img:nth-child("+k+")").clone().appendTo("#perm"+x);
+			$("#combos").append("<p></p>");
+			
 			x++;
+			
 			
 				}
 			}
 		}
+	
+		
 		
 	
 	});
 	
 	
+	$("#lvl2Drop").droppable({
+		hoverClass: "ui-state-active",
+		drop: function( event, ui ) {
+        	$( this ).css("background","yellow");
+        	//$("#greens").draggable("snap", "#drop");
+        	$(ui.draggable).appendTo($(this))
+                                           .css({position:'static', left:'0px', top:'0px'})
+                                           .draggable('option', 'disabled', true)
+                                           .css({position:'relative'});
+        	//$("#drop").append($("#greens"));
+        	//$(ui.draggable).draggable("disabled", "true");
+        	//$(this).css("z-index", "0");
+        }
+		});
+		$("#lvl2Drop").droppable("option","accept",".lvl1Perm");
+		
 	
+	
+		
 	
 	
 	
